232f59064b6d3641c88b159868779ccd90d2a840
trandainien1 <trandainien1@gmail.com>
2024-11-03 13:15:40 +0700

    change input date and time to type date and time instead of text

--- base/forms.py
+++ base/forms.py
@@ -2,11 +2,21 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import Task, CustomUser
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class TimeInput(forms.TimeInput):
+    input_type = 'time'
+
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = '__all__'
         exclude = ['user', 'state', 'completed']
+        widgets = {
+            'due_date': DateInput(),
+            'due_time': TimeInput()
+        }
     
 class MyUserCreationForm(UserCreationForm):
     class Meta:
